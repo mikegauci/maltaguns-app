590629db68b257ec481e454fea2be1e3f34a6c49
mikegauci <mikegauci@gmail.com>
2026-07-16 15:28:44 +0200

    Adjusted margin on past events title

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -302,7 +302,7 @@ export default function EventsPage() {
 
       {/* Past Events */}
       <div>
-        <h2 className="text-2xl font-bold mb-6">Past Events</h2>
+        <h2 className="text-2xl mt-8 font-bold mb-6">Past Events</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {!baseQuery.isLoading &&
             pastEvents.map(event => (
